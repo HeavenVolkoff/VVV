ffe54fade5c1e4c59e9d6fa879d25e56376a5328
HeavenVolkoff <HeavenVolkoff@users.noreply.github.com>
2015-11-26 13:47:47 -0200

    + Implemented User Creation + Added Bean Validator

--- src/Rest/Controller/UserController.java
+++ src/Rest/Controller/UserController.java
@@ -3,16 +3,15 @@ package Rest.Controller;
 import Rest.Model.tables.daos.UserDao;
 import Rest.Model.tables.pojos.User;
 import Server.DbFactory;
-import org.glassfish.hk2.api.PerLookup;
+import org.hibernate.validator.constraints.Email;
+import org.mindrot.jbcrypt.BCrypt;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 
-@PerLookup
-@Path("passageiro")
+@Path("user")
 public class UserController {
 
     @GET
@@ -28,5 +27,35 @@ public class UserController {
         }
 
         return null;
+    }
+
+    @POST
+    @Path("create")
+    @Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+    @Produces({MediaType.TEXT_PLAIN})
+    public String create(
+            @NotNull @FormParam("firstName") String firstName,
+            @NotNull @FormParam("lastName") String lastName,
+            @NotNull @FormParam("password") String password,
+            @Email @FormParam("email") String email
+    ) {
+        try(DbFactory.Db db = DbFactory.getInstance().open()){
+            byte[] passwordHash = BCrypt.hashpw(password, BCrypt.gensalt(12)).getBytes();
+            UserDao userDao = new UserDao(db.getConfiguration());
+            User user = new User();
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setEmail(email);
+            user.setPasswordHash(passwordHash);
+
+            userDao.insert(user);
+
+            return "ok";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "not ok";
+        }
     }
 }
